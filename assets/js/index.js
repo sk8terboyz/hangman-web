664a298383005e964f80218dbd0fa11885e648c9
Shane Kennedy <kennedy.shane@att.net>
2022-02-11 13:06:19 -0600

    Major updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,30 @@
 
 
 document.querySelector(".input-btn").addEventListener("click", guessLetter);
+document.addEventListener("keydown", function(e) {
+    if(e.key === 'Enter') {
+        guessLetter();
+    }
+});
+
 
 function guessLetter() {
     // Get the input when the button is pressed
     const input = document.querySelector("#input");
+    // Check for special characters as input
+    const format = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    //             ^                                       ^
+    console.log(format.test(input.value)); 
     // Check if the input is any of the 4 letters (hardcoded instead of a library due to instructions)
-    if(input.value == 'f' || input.value == 'i' || input.value == 'r' || input.value == 'e') {
-        console.log(input.value);   //temp
-        alert("good guess")
+    if(isNaN(input.value) && !format.test(input.value)){
+        if(input.value == 'f' || input.value == 'i' || input.value == 'r' || input.value == 'e') {
+            console.log(input.value);   //temp
+            alert("good guess")
+        } else {
+            failedGuess(input.value);
+        }
     } else {
-        failedGuess(input.value);
+        alert("You can only guess letters. Numbers & special characters are not allowed.")
     }
     input.value = "";
 }
@@ -18,9 +32,9 @@ function guessLetter() {
 // Called each time an incorrect letter is guessed
 function failedGuess(letter) {
     var change;
-    if(change= document.querySelector(".hidden")) {
+    if(change = document.querySelector(".hidden-img")) {
         // display the next part of the hangman
-        change.classList.remove("hidden");
+        change.classList.remove("hidden-img");
         // display incorrectly guessed letters
     }
     else {
